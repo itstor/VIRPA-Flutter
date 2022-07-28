d8969a5e036287806d8542701ae88f054ffb6231
itstor <ahmdthoriq5@gmail.com>
2022-07-28 07:17:08 +0700

    fix: api url

--- lib/constants/api_constant.dart
+++ lib/constants/api_constant.dart
@@ -1,6 +1,6 @@
 class Api {
-  static String head = 'https://6b22-103-120-66-232.ap.ngrok.io/api/deteksi';
+  static String head = 'http://virpa.rgtasty.com/api/deteksi';
   static String side = 'https://virpaflaskapp.azurewebsites.net/side';
   static String front = 'https://virpaflaskapp.azurewebsites.net/front';
-  static String save = 'https://6b22-103-120-66-232.ap.ngrok.io/api/save-data';
+  static String save = 'http://virpa.rgtasty.com/api/save-data';
 }
